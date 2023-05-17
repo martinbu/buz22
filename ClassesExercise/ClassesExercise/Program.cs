db60f921ce674268eb4ef2521deb1dac597e9548
Martin Burtscher <martin.burtscher@teslab.com>
2023-05-17 18:09:39 +0200

    Kaffeemaschinenbeispiel - Klassen

---
+++ ClassesExercise/ClassesExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ClassesExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kaffemaschine roteMaschine = new Kaffemaschine();
+            roteMaschine.Farbe = "Rot";
+
+            Kaffemaschine grüneMaschine = new Kaffemaschine();
+            grüneMaschine.Farbe = "Grün";
+
+            roteMaschine.ErstelleKleinenKaffee();
+            grüneMaschine.ErstelleKleinenKaffee();
+        }
+    }
+}
