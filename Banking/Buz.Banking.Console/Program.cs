7b28b2db34ac81273cefd6dc3fe61d8a2511d89c
Martin Burtscher <martin.burtscher@teslab.com>
2023-05-24 19:41:30 +0200

    added banking template

---
+++ Banking/Buz.Banking.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Buz.Banking.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sicherstellen, dass € zeichen auch ausgegeben wird
+            System.Console.OutputEncoding = System.Text.Encoding.Unicode;
+            
+            // new BankAccount("Vorname Nachname", 3500.50m);
+            // Run(bankAccount);
+        }
+
+        static void Run(IBankAccount bankAccount)
+        {
+            bankAccount.MakeDeposit(500.20m, "Geld von einem Freund");
+            bankAccount.MakeWithdrawal(22.50m, "Einkaufen Spar");
+
+            System.Console.WriteLine("Balance of {0}({1}): {2:c}", bankAccount.Owner, bankAccount.Number, bankAccount.Balance);
+        }
+    }
+}
---
+++ Banking/Buz.Banking/IBankAccount.cs
@@ -0,0 +1,14 @@
+﻿namespace Buz.Banking
+{
+    public interface IBankAccount
+    {
+        string Number { get; }
+        string Owner { get; }
+        decimal Balance { get; }
+
+        void MakeDeposit(decimal amount, string note, DateTime? date = null);
+        void MakeWithdrawal(decimal amount, string note, DateTime? date = null);
+
+        string ExportTransactionsAsCsv(DateTime from, DateTime to);
+    }
+}
