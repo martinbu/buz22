11e98147653a3535c0ba4d052db6f6ebdfbbe5ac
Martin Burtscher <martin.burtscher@teslab.com>
2023-05-28 07:31:27 +0200

    banking: added initial transaction implementation

---
+++ Banking/Buz.Banking/BankAccount.cs
@@ -0,0 +1,37 @@
+﻿namespace Buz.Banking
+{
+    public class BankAccount : IBankAccount
+    {
+        public string Number => throw new NotImplementedException();
+
+        public string Owner => throw new NotImplementedException();
+
+        public decimal Balance => throw new NotImplementedException();
+
+        private readonly List<Transaction> _Transaction;
+
+        public BankAccount()
+        {
+            _Transaction = new List<Transaction>();
+        }
+
+        public string ExportTransactionsAsCsv(DateTime from, DateTime to)
+        {
+            return "";
+        }
+
+        public void MakeDeposit(decimal amount, string note, DateTime? date = null)
+        {
+            if (date == null)
+            {
+                date = DateTime.Now;
+            }
+
+            _Transaction.Add(new Transaction(amount, note, date.Value));
+        }
+
+        public void MakeWithdrawal(decimal amount, string note, DateTime? date = null)
+        {
+        }
+    }
+}
---
+++ Banking/Buz.Banking/Transaction.cs
@@ -0,0 +1,16 @@
+﻿namespace Buz.Banking
+{
+    internal class Transaction
+    {
+        public Transaction(decimal amount, string note, DateTime time)
+        {
+            Amount = amount;
+            Note = note;
+            Time = time;
+        }
+
+        public decimal Amount { get; }
+        public string Note { get; }
+        public DateTime Time { get; set; }
+    }
+}
