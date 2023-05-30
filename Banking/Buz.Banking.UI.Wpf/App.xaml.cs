53cd5d6ab5995f267fdb6c31f0fec34719a995c7
Martin Burtscher <martin.burtscher@teslab.com>
2023-05-30 20:58:41 +0200

    banking: added initial wpf application

---
+++ Banking/Buz.Banking.UI.Wpf/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using System.Threading;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace Buz.Banking.UI.Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var culture = new CultureInfo("de-DE");
+
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+
+            FrameworkElement.LanguageProperty.OverrideMetadata(
+                typeof(FrameworkElement),
+                new FrameworkPropertyMetadata(
+             XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ Banking/Buz.Banking.UI.Wpf/Common/DelegateCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Buz.Banking.UI.Wpf.Common
+{
+    public sealed class RelayCommand : ICommand
+    {
+        private readonly Action _Execute;
+        private readonly Func<bool>? _CanExecute;
+
+        public event EventHandler? CanExecuteChanged;
+
+        public RelayCommand(Action execute)
+        {
+            ArgumentNullException.ThrowIfNull(execute);
+            _Execute = execute;
+        }
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            ArgumentNullException.ThrowIfNull(execute);
+            ArgumentNullException.ThrowIfNull(canExecute);
+
+            _Execute = execute;
+            _CanExecute = canExecute;
+        }
+
+        public void NotifyCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            if (_CanExecute == null) return true;
+
+            return _CanExecute.Invoke();
+        }
+
+        public void Execute(object? parameter)
+        {
+            _Execute();
+        }
+    }
+}
